06f9d9be0b07337d07b41903e5cee7bc5b52117f
Peter Pencov <pnpencov@gmail.com>
2024-08-04 13:31:29 +0300

    completed new project: shopping list

---
+++ shopping_list/script.js
@@ -0,0 +1,39 @@
+const inputText = document.querySelector("#input");
+const addBtn = document.querySelector("#add-item");
+const shoppingList = document.querySelector("#shopping-list");
+
+let deleteBtnList = []
+let shoppingListItems = []
+let itemWrapperList = []
+
+
+function addNewItem(text) {
+    const newItemWrapper = document.createElement("li");
+    newItemWrapper.className = "newItemWrapper";
+    const newItem = document.createElement("p");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn"
+    deleteBtn.innerText = "Delete";
+
+    deleteBtn.addEventListener("click", () => {
+        newItemWrapper.remove();
+        inputText.focus();
+    })
+
+    newItem.innerText = text;
+    newItemWrapper.appendChild(newItem);
+    newItemWrapper.appendChild(deleteBtn);
+    shoppingList.appendChild(newItemWrapper);
+}
+
+
+addBtn.addEventListener("click", () => {
+    if (inputText.value) {
+        addNewItem(inputText.value);
+    } else {
+        alert("please enter an item name.")
+    }
+    inputText.value = "";
+    inputText.focus();   
+})
+
