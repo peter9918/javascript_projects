081bbe7b1f27f1d35ae12846dc8ea1f8432834d0
Peter Pencov <pnpencov@gmail.com>
2024-08-27 18:51:53 +0300

    changed check win method, it was not working. Now it s ok. inside square eventListener added check for win, check for draw and switch turns. Decided it's not needed to create a Game object as well, I'll just do everything inside the event listener

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -3,21 +3,27 @@ const boardWrapper = document.querySelector("#board-wrapper");
 
 const Gameboard = (function() {
     const squares = ["","","","","","","","",""];
+    const winningCombinations = [
+        [0, 1, 2], // top row
+        [3, 4, 5], // middle row
+        [6, 7, 8], // bottom row
+        [0, 3, 6], // left column
+        [1, 4, 7], // middle column
+        [2, 5, 8], // right column
+        [0, 4, 8], // main diagonal
+        [2, 4, 6]  // anti-diagonal
+    ];
+    
     const checkWin = function() {
-        if (squares[0] && squares[0] === (squares[1] && squares[2]) ||
-            squares[3] && squares[3] === (squares[4] && squares[5]) ||
-            squares[6] && squares[6] === (squares[7] && squares[8]) ||
-            squares[0] && squares[0] === (squares[3] && squares[6]) ||
-            squares[1] && squares[1] === (squares[4] && squares[7]) ||
-            squares[2] && squares[2] === (squares[5] && squares[8]) ||
-            squares[0] && squares[0] === (squares[4] && squares[8]) ||
-            squares[2] && squares[2] === (squares[4] && squares[6])
-        ) {
-            return true;
-        } else {
-            return false;
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return true;
+            }
         }
+        return false;
     };
+    
     const checkDraw = function() {
         let draw = true; 
         Object.values(squares).forEach((val) => {
@@ -31,15 +37,18 @@ const Gameboard = (function() {
     return { squares, checkWin, checkDraw };
 })();
 
-function createPlayer(name, mark) {
+function createPlayer(name, mark, isTurn) {
     // place mark in gameboard square if square is empty
     const placeMark = function(square) {
-        if (Gameboard.squares[square] === "") {
             Gameboard.squares[square] = mark;
-        }
+            // place mark in display board as well 
+            Display.updateSquare(square);
     };
-    return {name, mark, placeMark};
+    return {name, mark, isTurn, placeMark};
 };
+
+const player1 = createPlayer("Player1", "X", true);
+const player2 = createPlayer("Player2", "O", false);
 
 const Display = (function() {
     const displayBoard = function(board) {
@@ -50,14 +59,36 @@ const Display = (function() {
             square.innerText = elem;
             square.setAttribute("data-count", counter);
             square.addEventListener("click", () => {
-                // update game board value
-                player1.placeMark(square.attributes["data-count"].nodeValue);
-                // update display value
-                updateSquare(square.attributes["data-count"].nodeValue);
+                // check if square is empty
+                if (square.innerText === "") {
+                    // update game board value
+                    console.log("!!!!!!!!!!")
+                    if (player1.isTurn) {
+                        player1.placeMark(square.attributes["data-count"].nodeValue);
+                        if (Gameboard.checkWin()) {
+                            console.log(`${player1.mark}'s win`)
+                            console.log(Gameboard.squares)
+                        } else if (Gameboard.checkDraw()) {
+                            console.log("draw");
+                        }
+                    } else {
+                        player2.placeMark(square.attributes["data-count"].nodeValue);
+                        if (Gameboard.checkWin()) {
+                            console.log(`${player2.mark}'s win`)
+                            console.log(Gameboard.squares)
+                        } else if (Gameboard.checkDraw()) {
+                            console.log("draw");
+                        }
+                    }
+                // switch turns
+                player1.isTurn = !player1.isTurn
+                player2.isTurn = !player2.isTurn
+                }
             });
             counter ++;
             boardWrapper.appendChild(square);
         });
+    };
 
     const updateSquare = function(square) {
         // select all square elements
@@ -65,22 +96,19 @@ const Display = (function() {
         // update display square inner text
         displaySquares[square].innerText = Gameboard.squares[square];
         };
-    };
-    return { displayBoard };
+    
+    return { displayBoard, updateSquare };
 })();
 
-const player1 = createPlayer("Player1", "X");
-const player2 = createPlayer("Player2", "O");
-
-
 Display.displayBoard(Gameboard.squares);
+
+// const Game = (function() {
+    
+// })
 
 // player1.placeMark(0);
 // player1.placeMark(3);
 // player1.placeMark(6);
-
-
-
 
 // console.log(Gameboard.squares);
 // console.log(Gameboard.checkWin())
